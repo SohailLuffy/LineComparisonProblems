566f8bdace7b0f2db53bb4833a42f7228ce707cc
SohailLuffy <sohailislammail97@gmail.com>
2022-11-13 22:24:32 +0530

    Calculate Length Of Both Lines And Compare Them

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace LC1
+namespace LC2
 {
-    internal class LC1
+    internal class Program
     {
         static void Main(string[] args)
         {
-            //Accepting Values For Line
+      
             Console.Write("Enter Value A1:");
             int a1 = Convert.ToInt32(Console.ReadLine());
 
@@ -19,8 +19,36 @@ namespace LC1
             Console.Write("Enter Value B2:");
             int b2 = Convert.ToInt32(Console.ReadLine());
 
-            double value = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
-            Console.WriteLine("Ans Is:" + value);
+            double ans1 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+            Console.WriteLine("Ans Is:" + ans1);
+            Console.WriteLine("------------------------------------------------------------------------------------");
+
+          
+            Console.Write("Enter Value C1:");
+            int c1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Value D1:");
+            int d1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Value C2:");
+            int c2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Value D2:");
+            int d2 = Convert.ToInt32(Console.ReadLine());
+
+            double ans2 = Math.Sqrt(Math.Pow((c2 - c1), 2) + Math.Pow((d2 - d1), 2));
+            Console.WriteLine("Ans Is:" + ans2);
+            Console.WriteLine("------------------------------------------------------------------------------------");
+
+   
+            if (ans1 == ans2)
+            {
+                Console.WriteLine("Both The Lines Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Both The Lines Are Not Equal");
+            }
         }
     }
 }
